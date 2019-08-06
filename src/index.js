c7b153cc057769a7d8ce7e30d2a8bb69cc1abea1
Jesse Neumann <jesseneumann@gmail.com>
2019-08-06 11:57:10 -0600

    ping server

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {ApolloClient} from 'apollo-client';
-import {InMemoryCache} from 'apollo-cache-inmemory';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import { createHttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
-import {Query, ApolloProvider} from 'react-apollo';
+import { Query, ApolloProvider } from 'react-apollo';
 import gql from 'graphql-tag';
 
 import Pages from './pages';
 import Authenticate from './authentication';
-import {typeDefs, resolvers} from './resolvers';
+import { typeDefs, resolvers } from './resolvers';
 
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 
+fetch('https://library-apollo.herokuapp.com/graphql');
+
 const httpLink = createHttpLink({
-  uri: 'https://library-apollo.herokuapp.com/graphql',
-})
+  uri: 'https://library-apollo.herokuapp.com/graphql'
+});
 
 const authLink = setContext((_, { headers }) => {
-
   const token = localStorage.getItem('token');
   return {
     headers: {
       ...headers,
-      authorization: token ? token : "none",
+      authorization: token ? token : 'none'
     }
-  }
+  };
 });
 
 const client = new ApolloClient({
@@ -40,8 +40,8 @@ const client = new ApolloClient({
 
 client.cache.writeData({
   data: {
-    isLoggedIn: !!localStorage.getItem('token'),
-  },
+    isLoggedIn: !!localStorage.getItem('token')
+  }
 });
 
 const IS_LOGGED_IN = gql`
@@ -53,8 +53,8 @@ const IS_LOGGED_IN = gql`
 ReactDOM.render(
   <ApolloProvider client={client}>
     <Query query={IS_LOGGED_IN}>
-      {({data}) => ( data && data.isLoggedIn ? <Pages /> : <Authenticate />)}
+      {({ data }) => (data && data.isLoggedIn ? <Pages /> : <Authenticate />)}
     </Query>
   </ApolloProvider>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
